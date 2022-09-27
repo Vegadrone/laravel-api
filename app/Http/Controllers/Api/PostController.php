a9522f29d7e7bfa9ddde8372f33e2806c3b6e67b
Vegadrone <spenaantonino@gmail.com>
2022-09-27 17:07:55 +0200

    api posts index exposition

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,8 +15,11 @@ class PostController extends Controller
      */
     public function index()
     {
+        $posts = Post::all();
         return response()->json([
-
+            'response' => true,
+            'count' => ($posts),
+            'results' => $posts,
         ]);
     }
 
