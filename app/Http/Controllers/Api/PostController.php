c828250b127a13e3476b304a0fba9ce509e36b81
Vegadrone <spenaantonino@gmail.com>
2022-09-27 17:50:11 +0200

    add json pagination

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -15,10 +15,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::with('user')->get();
+        $posts = Post::with('user')->paginate(10);
         return response()->json([
             'response' => true,
-            'count' => count($posts),
             'results' => $posts,
         ]);
     }
